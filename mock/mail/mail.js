be3f21c21ab98a70147bc2bb9480d4f6030e33c6
gongchizhou <gongchizhou@gmail.com>
2017-10-07 16:58:56 +0800

    update

--- mock/mail/mail.js
+++ mock/mail/mail.js
@@ -1,105 +1,105 @@
 module.exports = {
-    hasMore:true,
-    item:[
+    "hasMore":true,
+    "item":[
         {
-        	id:1,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":1,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:2,
-            title:"Birthday news",
-            author:"Kevin Carl",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:true,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thank you for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":2,
+            "title":"Birthday news",
+            "author":"Kevin Carl",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":true,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thank you for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:3,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":3,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:4,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":4,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:5,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":5,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:6,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+            "id":6,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:7,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":7,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."        
         },
         {
-        	id:8,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":8,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:9,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":9,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         },
         {
-        	id:10,
-            title:"Birthday news",
-            author:"Kevin Hopkins",
-            address:"Anthen@zimail.com",
-            time:"2:52pm",
-            isLike:false,
-            imgUrl:"https://unsplash.it/200/300/?random",
-            content:"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
+        	"id":10,
+            "title":"Birthday news",
+            "author":"Kevin Hopkins",
+            "address":"Anthen@zimail.com",
+            "time":"2:52pm",
+            "isLike":false,
+            "imgUrl":"https://unsplash.it/200/300/?random",
+            "content":"Thanks for sending the documents over. We’re good to go on this end - very exctited to start working with you."
         }
     ]
 }
--- mock/user/user.js
+++ mock/user/user.js
@@ -1,44 +1,44 @@
 module.exports = {
     item:[
         {
-        	id:1,
-            name:"Allen Hawkins",
-            email:"allen@zimail.com",
-            mobile:"15801039032",
-            tel:"010-8066732",
-            imgUrl:"https://unsplash.it/200/300/?random"
+        	"id":1,
+            "name":"Allen Hawkins",
+            "email":"allen@zimail.com",
+            "mobile":"15801039032",
+            "tel":"010-8066732",
+            "imgUrl":"https://unsplash.it/200/300/?random"
         },
         {
-        	id:2,
-            name:"Angela Carroll",
-            email:"angela@zimail.com",
-            mobile:"15801039032",
-            tel:"010-8066732",
-            imgUrl:"https://unsplash.it/200/300/?random"
+        	"id":2,
+            "name":"Angela Carroll",
+            "email":"angela@zimail.com",
+            "mobile":"15801039032",
+            "tel":"010-8066732",
+            "imgUrl":"https://unsplash.it/200/300/?random"
         },
         {
-        	id:3,
-            name:"Azury Murray",
-            email:"azury@zimail.com",
-            mobile:"15801039032",
-            tel:"010-8066732",
-            imgUrl:"https://unsplash.it/200/300/?random"
+        	"id":3,
+            "name":"Azury Murray",
+            "email":"azury@zimail.com",
+            "mobile":"15801039032",
+            "tel":"010-8066732",
+            "imgUrl":"https://unsplash.it/200/300/?random"
         },    
         {
-        	id:4,
-            name:"Babette Elliott",
-            email:"babette@zimail.com",
-            mobile:"15801039032",
-            tel:"010-8066732",
-            imgUrl:"https://unsplash.it/200/300/?random"
+        	"id":4,
+            "name":"Babette Elliott",
+            "email":"babette@zimail.com",
+            "mobile":"15801039032",
+            "tel":"010-8066732",
+            "imgUrl":"https://unsplash.it/200/300/?random"
         },    
         {
-        	id:5,
-            name:"Christina Ross",
-            email:"christina@zimail.com",
-            mobile:"15801039032",
-            tel:"010-8066732",
-            imgUrl:"https://unsplash.it/200/300/?random"
+        	"id":5,
+            "name":"Christina Ross",
+            "email":"christina@zimail.com",
+            "mobile":"15801039032",
+            "tel":"010-8066732",
+            "imgUrl":"https://unsplash.it/200/300/?random"
         }
     ]
 }
